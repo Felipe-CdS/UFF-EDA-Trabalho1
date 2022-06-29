36303ab230385b4bf9376b8d7fb5fb7e4ebc825a
Felipe-CdS <felipe32santos@hotmail.com>
2022-06-29 16:51:09 -0300

    i-node table norma fix'd

--- libs/inode_table.c
+++ libs/inode_table.c
@@ -6,7 +6,7 @@
 /*   By: fcoutinh <felipe_coutinho@id.uff.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 17:43:21 by fcoutinh          #+#    #+#             */
-/*   Updated: 2022/06/27 19:26:38 by fcoutinh         ###   ########.fr       */
+/*   Updated: 2022/06/29 16:50:18 by fcoutinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,13 @@
 
 t_itable	*it_new(int id, char *filename)
 {
-	t_itable *new;
+	t_itable	*new;
 
 	if (!id)
 		return (NULL);
 	new = (t_itable *) malloc(sizeof(t_itable));
 	if (!new)
 		return (NULL);
-
 	new->id = id;
 	new->filename = filename;
 	new->next = NULL;
@@ -37,13 +36,13 @@ t_itable	*it_last(t_itable *start)
 	return (it_last(start->next));
 }
 
-void		it_add_front(t_itable **start, t_itable  *new)
+void	it_add_front(t_itable **start, t_itable *new)
 {
 	new->next = *start;
 	*start = new;
 }
 
-void		it_add_back(t_itable **start, t_itable  *new)
+void	it_add_back(t_itable **start, t_itable *new)
 {
 	t_itable	*last;
 
@@ -54,11 +53,11 @@ void		it_add_back(t_itable **start, t_itable  *new)
 		*start = new;
 }
 
-void		it_delone(t_itable **start, t_itable *target)
+void	it_delone(t_itable **start, t_itable *target)
 {
 	t_itable	*x;
 	t_itable	*previous;
-	
+
 	x = (*start);
 	while (x && x != target)
 	{
@@ -71,10 +70,11 @@ void		it_delone(t_itable **start, t_itable *target)
 		free(x);
 	}
 }
-void		it_clear(t_itable **start)
+
+void	it_clear(t_itable **start)
 {
-	t_itable *p;
-	t_itable *q;
+	t_itable	*p;
+	t_itable	*q;
 
 	p = (*start);
 	while (p)
@@ -86,27 +86,27 @@ void		it_clear(t_itable **start)
 	*start = NULL;
 }
 
-int			it_size(t_itable *start)
+int	it_size(t_itable *start)
 {
 	if (!start)
 		return (0);
 	return (it_size(start->next) + 1);
 }
 
-void		it_print(t_itable **start)
+void	it_print(t_itable **start)
 {
-	t_itable *p;
+	t_itable	*p;
 
 	p = (*start);
-	printf("+-----------------------------------------------------------------------+\n");
-	printf("|\t\t\t\ti-nodes Table\t\t\t\t|\n");
-	printf("+-----------------------------------------------------------------------+\n");
-	printf("| ID | %-65s|\n", "FILENAME");
-	printf("+-----------------------------------------------------------------------+\n");
-	while(p)
+	printf("+---------------------------------------+\n");
+	printf("|\t\ti-nodes Table\t\t|\n");
+	printf("+---------------------------------------+\n");
+	printf("| ID | %-33s|\n", "FILENAME");
+	printf("+---------------------------------------+\n");
+	while (p)
 	{
-	printf("| %02d | %-65s|\n", p->id, p->filename);
-	p = p->next;
+		printf("| %02d | %-33s|\n", p->id, p->filename);
+		p = p->next;
 	}
-	printf("+-----------------------------------------------------------------------+\n");
+	printf("+---------------------------------------+\n");
 }
